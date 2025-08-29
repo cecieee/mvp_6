d5e1a54a61836c2873c7e2b6cbaa54f54b3db844
Alwin Saji <alwinsaji4.cgnr@gmail.com>
2025-08-29 21:04:52 +0530

    LEADERBOARD

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'; // Assuming you want to import the Leaderboard component
+
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="flex justify-center gap-2">
+      <Link to="/">Home</Link>
+      <Link to="/leaderboard">Leaderboard</Link>
+      </div>
+    </div>
   )
 }
